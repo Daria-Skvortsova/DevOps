0746e2d253d20a5c20e466e85c3818106b811d1d
DSkvortsova <dkvarts03155@mail.ru>
2024-12-07 11:18:42 +0300

    Add laba.groovy

---
+++ laba.groovy
@@ -0,0 +1,30 @@
+pipeline {
+    agent any
+
+    stages {
+        stage('Deploy') {
+            steps {
+                ansiblePlaybook(
+                    playbook: './ansible/mainPlayBook.yml'
+                    inventory: '.ansible/inventory/stend/hosts'
+                    hostKeyChecking: false
+                    disableHostKeyChecking: true
+                    credentialsId: '~/.ssh.id_rsa'
+                    tags:'deploy'
+                )
+            }
+        }
+        stage('Start') {
+            steps {
+                ansiblePlaybook(
+                    playbook: './ansible/mainPlayBook.yml'
+                    inventory: '.ansible/inventory/stend/hosts'
+                    hostKeyChecking: false
+                    disableHostKeyChecking: true
+                    credentialsId: '~/.ssh.id_rsa'
+                    tags:'start'
+                )
+            }
+        }
+    }
+}
